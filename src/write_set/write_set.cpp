a5cb3f8838251e6753cc927b011aaf7ed028249e
Paolo Joseph Baioni <paolojoseph.baioni@mail.polimi.it>
2020-04-26 13:28:19 +0000

    added: 3 x-init, omega
    Now, modifying the file parameters.pot, it's possible to:
     - write sets with linspaced, uniform random and gaussian random
       distributed x points
     - change the modulated frequence
    Naming is handled automatically from write_set.cpp, an update
    is required in main.cpp for automatic handling in the reading phase.

--- src/write_set/write_set.cpp
+++ src/write_set/write_set.cpp
@@ -4,20 +4,18 @@
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
+#include <random>
 #include "GetPot.hpp"
 
 using namespace std;
 using Eigen::VectorXd;
 
-//compile with:
-//g++ -std=c++14 -I$mkEigenInc -I./../../include/ write_set.cpp
-
 
 constexpr double pi=4.*std::atan(1.);
 
-void write_set(string filename, const VectorXd & X, const char & separator = ' '){
+void write_set(string filename, const VectorXd & X, const double omega, const char & separator = ' '){
 	ofstream ofs(filename);
-	auto u = [] (const double & x) {return exp(-x*x*5)*sin(5*pi*x);};	
+	auto u = [omega] (const double & x) {return exp(-x*x*5)*sin(omega*pi*x);};	
 	for(size_t i=0; i<X.rows(); ++i)
 		ofs<<X(i)<<separator<<u(X(i))<<"\n";
 	ofs.close();
@@ -31,6 +29,26 @@ ostream & help(){
 	return cout;
 }
 
+void init_spacing(VectorXd & X, const size_t n, const size_t xspacing, string & filename){
+	switch(xspacing){
+		default:
+			X.setLinSpaced(n,-1.5,1.5);
+			filename += "Linspaced";
+			break;
+		case 1:
+			X = VectorXd::Random(n);
+			X = X * 1.5;
+			filename += "Uniform";
+			break;
+		case 2:
+		 	default_random_engine gen;
+		  normal_distribution<double> dist(0.,.75);
+		  auto gaussian = [&dist, &gen] (double) {return dist(gen);};
+			X = VectorXd::NullaryExpr(n, gaussian);
+			filename += "Normal";
+			break;
+	}
+}
 
 int main(int argc, char** argv){
 	GetPot commandline(argc,argv);
@@ -39,15 +57,24 @@ int main(int argc, char** argv){
 		return 0;
 	}
 	string param_filename = commandline.follow("./../../data/parameters.pot",2,"-p","--parameters");
+	
 	GetPot datafile(param_filename.c_str());
 	const size_t ntrain = datafile("ntraindata", 70);
 	const size_t ntest = datafile("ntestdata", 30);
+	const size_t xspacing = datafile("xspacing",0);
+	const double omega = datafile("omega",5.);
+	
+	string filename{"./../../data/TrainingSet"+to_string(ntrain)};
 	VectorXd X;
-	X.setLinSpaced(ntrain,-1.5,1.5);
-	string file1{"./../../data/TrainingSet"+to_string(ntrain)}, file2{"./../../data/TestSet"+to_string(ntest)};
-	write_set(file1+".dat",X);
-	X.setLinSpaced(ntest,-1.5,1.5);
-	write_set(file2+".dat",X);
+	init_spacing(X, ntrain, xspacing, filename);
+	filename += to_string(omega);
+	write_set(filename+".dat", X, omega);
+	
+	filename = "./../../data/TestSet"+to_string(ntest);
+	init_spacing(X, ntest, xspacing, filename);
+	filename += to_string(omega);
+	write_set(filename+".dat", X, omega);
+	
 	cout<<"Done"<<endl;
 	return 0;
 }
